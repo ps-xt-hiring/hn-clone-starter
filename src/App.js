19e7f6d193974cc19c8a03ace1739c321f6f887f
Ankush Saini <anksaini1@publicisgroupe.net>
2019-10-31 15:28:15 +0530

    remove error boundary

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import './css/newsFeed.scss';
 import NewsFeed from './components/newsFeed';
 import NotFound from './components/notFound';
-import ErrorBoundary from './errorBoundary';
+// import ErrorBoundary from './errorBoundary';
 
 const App = () => (
-  <ErrorBoundary>
-    <Switch>
-      <Route path="/" exact component={NewsFeed} />
-      <Route path="/news" exact component={NewsFeed} />
-      <Route path="*" render={NotFound} />
-    </Switch>
-  </ErrorBoundary>
+  <Switch>
+    <Route path="/" exact component={NewsFeed} />
+    <Route path="/news" exact component={NewsFeed} />
+    <Route path="*" render={NotFound} />
+  </Switch>
 );
 
 export default App;
