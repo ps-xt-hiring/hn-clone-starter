c0cef4d387314d9df14d112b5065cb30e6fc51a9
Salil Kapoor <salilkapoor03@gmail.com>
2019-11-01 16:49:52 +0530

    Hacker-News: Few more errors resolved.

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const app = (
+ReactDOM.render((
   <BrowserRouter>
     <App />
   </BrowserRouter>
-);
-
-ReactDOM.render(app, document.getElementById('root'));
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
