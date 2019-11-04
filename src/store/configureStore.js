7cd5d1f4d224433379e3112ef8d3e0ac329b6b9e
ankush <saini.ankush29@gmail.com>
2019-11-04 23:35:46 +0530

    remove logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,20 +7,8 @@ import ReduxThunk from 'redux-thunk';
 
 import newsFeedReducer from './newsFeedReducer';
 
-/**
- * Logs all actions and states after they are dispatched.
- */
-const logger = store => next => (action) => {
-  console.group(action.type);
-  console.info('dispatching', action);
-  const result = next(action);
-  console.log('next state', store.getState());
-  console.groupEnd();
-  return result;
-};
-
 // if you're also using redux-thunk, add it as a middleware
-const createStoreWithMiddleware = compose(applyMiddleware(ReduxThunk, logger))(
+const createStoreWithMiddleware = compose(applyMiddleware(ReduxThunk))(
   createStore,
 );
 
