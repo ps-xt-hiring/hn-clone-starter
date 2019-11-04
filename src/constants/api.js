921d3df7e7c3fe2a460e6cb18b3480f707f6dd38
kirtisharma1 <55777334+kirtisharma1@users.noreply.github.com>
2019-11-04 13:02:15 +0530

    http to https

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
-export const GET_ITEM = 'http://hn.algolia.com/api/v1/items/';
-export const GET_NEWS_FEED = 'http://hn.algolia.com/api/v1/search?tags=front_page';
+export const GET_ITEM = 'https://hn.algolia.com/api/v1/items/';
+export const GET_NEWS_FEED = 'https://hn.algolia.com/api/v1/search?tags=front_page';
