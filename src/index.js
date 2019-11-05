317cda8f926fd471b08b8936c7fd5b785d11362e
ankush <saini.ankush29@gmail.com>
2019-11-05 09:23:08 +0530

    remove index css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { store } from './store/configureStore';
