e948e23f3d2c9684b34324417bc2cd54c883cc3e
Rohit Kumar <rohkumar30@publicisgroupe.net>
2019-11-06 12:44:01 +0530

    fixed tobaseurl regex

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
 import { formatDistance, subDays } from 'date-fns';
 
-export const toBaseURL = fullURL => fullURL && fullURL.replace(/(http(s)?:\/\/)|(\/.*){1}/g, '');
+export const toBaseURL = fullURL => fullURL && fullURL.replace(/(http(s)?:\/\/(www\.)?)|(\/.*){1}/g, '');
 
 export const getFormattedDate = date => formatDistance(subDays(new Date(date), 0), new Date());
