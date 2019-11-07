40e7978092e9db28e1f3d9bf1c320a665c1a85cf
Vipul Sharma <vipsharm2@publicisgroupe.net>
2019-11-07 13:16:13 +0530

    lint issue resolved

--- src/utils/manipulate.js
+++ src/utils/manipulate.js
@@ -1,6 +1,7 @@
 const fetchDomainName = (str) => {
     let domain;
     if(str && str !== null) {
+        /* eslint prefer-destructuring: ["error", {VariableDeclarator: {object: true}}] */
         domain = str.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];
     }
     return domain;
