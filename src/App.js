7636d62498797d4be61945f9982be464cbd09e45
Vishwesh <vishwesh.jah@medtronic.com>
2019-11-08 17:26:54 +0530

    lint issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Container } from 'reactstrap';
+import { store } from './redux/store';
 import './App.scss';
 import HomePage from './pages/homepage';
 
 function App() {
   return (
     <Container className="main-container">
-      <HomePage />
+      <Provider store={store}>
+        <HomePage />
+      </Provider>
     </Container>
   );
 }
