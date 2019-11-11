446371e517217d4a14895db23d5598f8d9ee94c8
Manideep Mittal <manmitta2@publicisgroupe.net>
2019-11-11 13:33:30 +0530

    redux dev tool enhancer added

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,16 @@
-import { createStore, applyMiddleware  } from 'redux';
+import { createStore, applyMiddleware, compose   } from 'redux';
 import rootReducer from '../reducer/index';
 import thunk from 'redux-thunk';
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  );
 
 export  function configureStore(initialState) {
     return createStore(
         rootReducer, 
         initialState,
+        enhancers,
         applyMiddleware(thunk)
     );
 }
+
