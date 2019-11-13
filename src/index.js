9b2c53fdbbd1d41bd58a54f6804e38e5742efc52
zeeshan <zahmad@sapient.com>
2019-11-13 20:47:01 +0530

    eslint issue fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
