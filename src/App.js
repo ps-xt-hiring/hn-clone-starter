c30481fa8990c8d773d5cfa1a9a080b1caecc478
ritugupta64 <gritu64@gmail.com>
2019-11-14 01:45:14 +0530

    requied-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-import { BrowserRouter as Router,
-
-  Route,
-  } from 'react-router-dom';
-import ListingUI from "./components/Listing/ListingUI"
-
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ListingUI from './components/Listing/ListingUI';
 
 function App() {
   return (
     <div className="container">
        <Router>
-     <Route exact path="/" component = {ListingUI} />
-     <Route exact path="/news" component = {ListingUI} />
+     <Route exact path="/" component={ListingUI} />
+     <Route exact path="/news" component={ListingUI} />
    </Router>
     </div>
-  
   );
 }
-
 export default App;
