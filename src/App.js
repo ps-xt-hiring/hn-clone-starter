26bbea51dc4efa70aad5e30b66f4917cdaad00fd
zeeshan <zahmad@sapient.com>
2019-11-13 20:47:01 +0530

    eslint issue fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from "react";
-import configureStore from "./redux/store/configureStore";
-import { Provider } from "react-redux";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import ConnectedLanding from "./components/module/feedContainer";
-import "./App.scss";
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './redux/store/configureStore';
+import ConnectedLanding from './components/module/feedContainer';
+import './App.scss';
 
 const store = configureStore();
-const App = () => {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <Router>
-          <div className="wrapper">
-            <Switch>
-              <Route exact path="/" component={ConnectedLanding} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    </Provider>
-  );
-};
+const App = () => (
+  <Provider store={store}>
+    <div className="App">
+      <Router>
+        <div className="wrapper">
+          <Switch>
+            <Route exact path="/" component={ConnectedLanding} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  </Provider>
+)
 export default App;
