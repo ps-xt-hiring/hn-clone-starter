09cb51642fc6d88bcba0d4230863739fae1900e7
aditimalhotra19 <aditimalhotra19@gmail.com>
2019-11-20 11:12:36 +0530

    Update Feeds.js

--- Feeds.js
+++ Feeds.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react';
 import FeedsData from './FeedsData';
 
 class Feeds extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
     return (
       <div className="feeds">
