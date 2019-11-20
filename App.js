d42c12918ac96dc513be7041895388ea8275b368
aditimalhotra19 <aditimalhotra19@gmail.com>
2019-11-20 11:11:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react';
 import Feeds from './Feeds';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
     return <Feeds />;
   }
