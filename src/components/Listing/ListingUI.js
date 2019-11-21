e19c65aaff08debf91a661f828e0b68b40044075
Ritu Gupta <ritgupta5@publicisgroupe.net>
2019-11-21 08:50:33 +0530

    feedback03

--- src/components/Listing/ListingUI.js
+++ src/components/Listing/ListingUI.js
@@ -1,32 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import { fetchProducts, hideItems, upVoteItems } from '../../redux/action';
 import Listing from './listing';
 import ListHeader from '../Header/header';
 
 class ListingUI extends Component {
   componentDidMount() {
-    const { productReducer, fetchProductsAction } = this.props;
+    const { productReducer, fetchProducts } = this.props;
     const page = productReducer.pagination;
-    fetchProductsAction(page);
+    fetchProducts(page);
   }
 
   ShowMoreItems = () => {
-    const { productReducer, history, fetchProductsAction } = this.props;
+    const { productReducer, history, fetchProducts } = this.props;
     const page = productReducer.pagination + 1;
     history.push(`/news?page=${page}`);
-    fetchProductsAction(page);
+    fetchProducts(page);
   };
 
   hideItem = (objId) => {
-    const { hideItemsAction, productReducer } = this.props;
-    hideItemsAction(objId, productReducer.items);
+    const { hideItems, productReducer } = this.props;
+    hideItems(objId, productReducer.items);
   };
 
   upVote = (objId) => {
-    const { upVoteItemsAction, productReducer } = this.props;
-    upVoteItemsAction(objId, productReducer.items);
+    const { upVoteItems, productReducer } = this.props;
+    upVoteItems(objId, productReducer.items);
   };
 
   render() {
@@ -53,23 +52,12 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchProductsAction: page => dispatch(fetchProducts(page)),
-  hideItemsAction: (objId, item) => dispatch(hideItems(objId, item)),
-  upVoteItemsAction: (objId, item) => dispatch(upVoteItems(objId, item)),
+  fetchProducts: page => dispatch(fetchProducts(page)),
+  hideItems: (objId, item) => dispatch(hideItems(objId, item)),
+  upVoteItems: (objId, item) => dispatch(upVoteItems(objId, item)),
 });
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
 )(ListingUI);
-
-
-ListingUI.propTypes = {
-  productReducer: PropTypes.oneOfType([PropTypes.array]).isRequired,
-  hideItemsAction: PropTypes.func.isRequired,
-  upVoteItemsAction: PropTypes.func.isRequired,
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
-  }).isRequired,
-  fetchProductsAction: PropTypes.oneOfType([PropTypes.array]).isRequired,
-};
