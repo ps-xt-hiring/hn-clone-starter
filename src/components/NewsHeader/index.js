2bc43ed4852a6f3a051783adad49222da9554d31
Rahul Tagra <rahtagra@publicisgroupe.net>
2019-12-02 12:56:55 +0530

    Lighthouse score improved

--- src/components/NewsHeader/index.js
+++ src/components/NewsHeader/index.js
@@ -10,15 +10,16 @@ const Logo = styled.span`
   border: 1px solid #fff;
 `;
 
-const Header = styled.header`
+const Header = styled.h3`
   background-color: #ff6600;
   padding: 10px 10px 9px 10px;
   display: flex;
   align-items: center;
   color: #fff;
+  margin:0
   header > a {
   text-decoration: none;
-  }
+ }
   button{
     font-size: 14px;
     line-height: 12px;
@@ -38,13 +39,13 @@ const NewsHeader = (props) => {
   const {sortBy} = props;
   return (
   <Header>
-    <header className="newsHeader">
+    <div className="newsHeader">
       <Link to="/">
         <Logo>Y</Logo>
       </Link>
       <button onClick = { () => props.sortNews(1, props.newsList) } ><span className={sortBy === 1 ? "tab-selected" : null}>top</span></button>|
       <button onClick = { () => props.sortNews(2, props.newsList) } ><span className={sortBy === 2 ? "tab-selected" : null}>new</span></button>
-    </header>
+    </div>
   </Header>
 );
 }
