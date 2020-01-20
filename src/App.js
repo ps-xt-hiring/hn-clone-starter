acee81d564178c7353ad3859b826f9e9a5f708e4
Vishal Gupta <visgupta10@publicisgroupe.net>
2020-01-20 19:01:57 +0530

    hookify the code from classful approach.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import StoryList from './components/StoryList';
 import Header from './components/Header';
 
-class App extends React.Component {
-  constructor(props, state) {
-    super(props, state);
+function App() {
 
-    this.state = {
-      currentView: 'top',
-    };
+  let [view, setView] = useState('top');
 
-    this.handleViewChange = this.handleViewChange.bind(this);
+  function handleViewChange(view) {
+    setView(view);
   }
 
-  handleViewChange(view) {
-    this.setState({
-      currentView: view,
-    });
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
 
+        <div className="Container">
+          <Header onViewChange={handleViewChange} />
+          <StoryList view={view} />
+        </div>
 
-  render() {
-    const { currentView } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-
-          <div className="Container">
-            <Header onViewChange={this.handleViewChange} />
-            <StoryList view={currentView} />
-          </div>
-
-        </header>
-      </div>
-    );
-  }
+      </header>
+    </div>
+  );
 }
 
 export default App;
