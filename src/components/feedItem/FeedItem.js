dc4bdf33aa311071fcd9bee5937b84d45725fbf2
Rahul Sharma <rahsharm19@publicisgroupe.net>
2020-02-18 14:56:03 +0530

    added eta method to action

--- src/components/feedItem/FeedItem.js
+++ src/components/feedItem/FeedItem.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import { HIDE_LABEL } from '../../constants';
 import './feedItem.scss';
+import fromNow from '../../actions/timeDiff';
 
 export default function FeedItem(props) {
   const {
@@ -26,52 +27,7 @@ export default function FeedItem(props) {
     const arr = url.split('/');
     return arr[2];
   };
-  function fromNow(date) {
-    var seconds = Math.floor((new Date() - date) / 1000);
-    var years = Math.floor(seconds / 31536000);
-    var months = Math.floor(seconds / 2592000);
-    var days = Math.floor(seconds / 86400);
-  
-    if (days > 548) {
-      return years + ' years ago';
-    }
-    if (days >= 320 && days <= 547) {
-      return 'a year ago';
-    }
-    if (days >= 45 && days <= 319) {
-      return months + ' months ago';
-    }
-    if (days >= 26 && days <= 45) {
-      return 'a month ago';
-    }
-  
-    var hours = Math.floor(seconds / 3600);
-  
-    if (hours >= 36 && days <= 25) {
-      return days + ' days ago';
-    }
-    if (hours >= 22 && hours <= 35) {
-      return 'a day ago';
-    }
-    
-    var minutes = Math.floor(seconds / 60);
-  
-    if (minutes >= 90 && hours <= 21) {
-      return hours + ' hours ago';
-    }
-    if (minutes >= 45 && minutes <= 89) {
-      return 'an hour ago';
-    }
-    if (seconds >= 90 && minutes <= 44) {
-      return minutes + ' minutes ago';
-    }
-    if (seconds >= 45 && seconds <= 89) {
-      return 'a minute ago';
-    }
-    if (seconds >= 0 && seconds <= 45) {
-      return 'a few seconds ago';
-    }
-  }
+  const timeElapsed = fromNow(new Date(feedItem.created_at));
   return (
     <article className={`feed-item  ${(order % 2 === 0 ? 'even' : 'odd')}`}>
       <span className="feed-item__comment-count">{feedItem.num_comments}</span>
@@ -85,7 +41,7 @@ export default function FeedItem(props) {
           ) by&nbsp;
           <b>{feedItem.author}</b>
           &nbsp;
-         {fromNow(new Date(feedItem.created_at))}
+         {timeElapsed}
         </span>
         &nbsp;[
         <button type="button" className="btn-empty feed-item__hide" onClick={() => hideFeedItem(feedItem)}>
