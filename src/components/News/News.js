194a8d594d905f1b59238ad39f224730e42abd18
Ashish007 <Ashish007>
2020-02-20 16:01:34 +0530

    test audit

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -31,7 +31,7 @@ export default class News extends React.Component {
   componentDidMount() {
     fetch(
       "https://hn.algolia.com/api/v1/search_by_date?tags=story&page=" +
-        this.state.pageCount
+        this.state.pageCount,
     )
       .then(response => response.json())
       .then(data => {
