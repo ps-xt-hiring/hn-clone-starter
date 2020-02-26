28ee8c097abd800d67890fe7dd846ff1e2730edc
Ashish007 <Ashish007>
2020-02-26 08:58:16 +0530

    added comments

--- src/static/constants/apiConstants.js
+++ src/static/constants/apiConstants.js
@@ -1,4 +1,4 @@
-// configuration file for APIs
+// configuration file for APIs and URLs
 const urlConfilg = {
   url: "https://hn.algolia.com/api/v1/search_by_date?tags=story&page=",
   logoUrl: "logo.gif"
