818a401358636b51191e700d6f17af3d00a8ad79
Rishabh Agrawal <rishabh.agrawal@publicissapient.com>
2020-03-19 11:56:19 +0530

    eslint error fix in news-list file

--- src/components/news-list/news-list.js
+++ src/components/news-list/news-list.js
@@ -4,11 +4,14 @@ import axios from 'axios';
 import NewsItem from '../news-item/news-item';
 
 export default class NewsList extends React.Component {
-  state = {
-    pageNo: 1,
-    newsList: [],
-    loading: false,
-    loadMore: false,
+  constructor() {
+    super();
+    this.state = {
+      pageNo: 1,
+      newsList: [],
+      loading: false,
+      loadMore: false,
+    };
   }
 
   componentDidMount() {
@@ -17,20 +20,20 @@ export default class NewsList extends React.Component {
 
   getNewsListData() {
     axios.get(`http://hn.algolia.com/api/v1/search_by_date?page=${this.state.pageNo}&numericFilters=num_comments>10,points>10`)
-      .then(res => {
+      .then((res) => {
         let result = res.data.hits;
         let hideNewsList = localStorage.getItem('hideNewsList');
         if (hideNewsList) {
           hideNewsList = JSON.parse(hideNewsList);
-          result = result.filter((val, key) => {
-            return hideNewsList.indexOf(val.objectID) === -1
-          });
+          result = result.filter(val => hideNewsList.indexOf(val.objectID) === -1);
         }
-        const lastNewsList = [...this.state.newsList];
 
-        const newsList = lastNewsList.concat(result);
+        const { newsList } = this.state;
+        const lastNewsList = [...newsList];
+
+        const updatedNewsList = lastNewsList.concat(result);
         this.setState({
-          newsList,
+          newsList: updatedNewsList,
           loading: true,
           loadMore: false,
         });
@@ -38,51 +41,50 @@ export default class NewsList extends React.Component {
   }
 
   loadMore() {
+    const { pageNo } = this.state;
     this.setState({
-      pageNo: this.state.pageNo + 1,
-      loadMore: true
+      pageNo: pageNo + 1,
+      loadMore: true,
     });
     this.getNewsListData();
   }
 
   hideItem(data) {
-    let objectId = data.data.objectID;
+    const objectId = data.data.objectID;
     let hideNewsList = localStorage.getItem('hideNewsList');
     if (hideNewsList) {
       hideNewsList = JSON.parse(hideNewsList);
     } else {
       hideNewsList = [];
     }
-    hideNewsList.push(objectId)
+    hideNewsList.push(objectId);
     localStorage.setItem('hideNewsList', JSON.stringify(hideNewsList));
-    let newsList = [...this.state.newsList];
-    newsList = newsList.filter((val, key) => {
-      return val.objectID !== objectId
-    });
+    const { newsList } = this.state;
+    let filteredNewsList = [...newsList];
+    filteredNewsList = newsList.filter(val => val.objectID !== objectId);
     this.setState({
-      newsList
-    })
+      newsList: filteredNewsList,
+    });
   }
 
   render() {
+    const { newsList, loading, loadMore } = this.state;
     return (
       <section className="news-list-wrapper">
         <ul className="news-list">
-          {this.state.newsList.map((val, key) => {
-            return (
-              <NewsItem key={val.created_at_i} hideItem={($event) => this.hideItem($event)} data={val} />
-            )
-          })}
+          {newsList.map(val => (
+            <NewsItem key={val.created_at_i} hideItem={$event => this.hideItem($event)} newsData={val} />
+          ))}
         </ul>
         {
-          this.state.loading ? (
-            this.state.loadMore ?
-              <div className="load-more-btn">Loading..</div>
-              :
-              <div className="load-more-btn" onClick={() => this.loadMore()}>More</div>
-          ) : null
+          loading && loadMore ? <div className="load-more-btn">Loading..</div> : null
+        }
+        {
+          loading && !loadMore
+            ? <button onClick={() => this.loadMore()} onKeyDown={this.handleClick} type="button" className="load-more-btn">More</button>
+            : null
         }
       </section>
-    )
+    );
   }
 }
