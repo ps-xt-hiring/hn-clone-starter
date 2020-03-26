7d765645b1cc509bf05b29d9e479e6fcd67e0d63
Tanya Jain 2 <tanjain@publicisgroupe.net>
2020-03-26 17:51:16 +0530

    code commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+
+import Header from './Header/Header.component';
+import NewsFeed from './NewsFeed/NewsFeed.container';
 
 function App() {
+  const [pageNo, setPageNo] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Publicis Sapient - XT hiring challenge!!</h1>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload. Refactor at will, but please do not
-          modify the entry point
-          {' '}
-          <code>index.js</code>
-.
-        </p>
-      </header>
+    <div className='App'>
+      <Header />
+      <NewsFeed pageNo={pageNo} />
+      <button
+        className='btn btn-secondary m-2'
+        type='button'
+        onClick={() => {
+          console.log('tanya');
+          setPageNo(pn => pn + 1);
+        }}
+      >
+        More Feeds
+      </button>
     </div>
   );
 }
-
 export default App;
