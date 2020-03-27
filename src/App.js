6c10a2b1885da332bfdc0ccdde86e2e7b440b849
Satyam Nema <satyam.mca12@gmail.com>
2020-03-27 08:44:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 
-import "./App.css";
-import NewsFeed from "./components/NewsFeed";
+import './App.css';
+import NewsFeed from './components/NewsFeed';
 
 function App() {
   return (
