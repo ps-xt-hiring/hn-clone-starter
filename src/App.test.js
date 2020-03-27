aa1a5249e67858620b61e7dc9c36ca7e7c154333
Satyam Nema <satyam.mca12@gmail.com>
2020-03-27 14:16:56 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NewsFeed from './components/NewsFeed';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App ><NewsFeed/></App>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
