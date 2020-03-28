a5c1907e38763d8826d9ed65a2766fc415a15ab3
Harpreet Chawla <harpreet.chawla@publicissapient.com>
2020-03-28 22:28:21 +0530

    review comments changes

--- src/NewsListComponent.js
+++ src/NewsListComponent.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable no-return-assign */
 /* eslint-disable no-param-reassign */
 /* eslint-disable max-len */
@@ -12,7 +13,6 @@ class NewsListComponent extends Component {
     super(props);
     this.state = {
       newsList: [],
-      hiddenListArray: [],
       currentPage: 1,
     };
   }
@@ -20,21 +20,12 @@ class NewsListComponent extends Component {
   async componentDidMount() {
     const { data } = await axios.get(baseQueryUrl);
     const hiddenItems = JSON.parse(localStorage.getItem('hiddenListArray'));
-    if (hiddenItems) {
-      const objIdsArr = [];
-      for (let i = 0; i < hiddenItems.length; i += 1) {
-        objIdsArr.push(hiddenItems[i].objectID);
-      }
-      for (let a = 0; a < objIdsArr.length; a += 1) {
-        for (let b = 0; b < data.hits.length; b += 1) {
-          if (data.hits[b].objectID === objIdsArr[a]) {
-            data.hits.splice(b, 1);
-          }
-        }
-      }
+    this.updateHiddenList(hiddenItems, data);
+    let newData = data.hits;
+    newData.map(obj => (obj.upvote_count = 0));
+    if (localStorage.getItem('upvoteList')) {
+      newData = this.updateVoteList(newData);
     }
-    const newData = data.hits;
-    newData.map(o => (o.upvote_count = 0));
     this.setState({ newsList: newData });
   }
 
@@ -44,19 +35,65 @@ class NewsListComponent extends Component {
     currentPage += 1;
     this.setState({ currentPage });
     const { data } = await axios.get(`${baseQueryUrl}?query=page=${currentPage}`);
-    const updatedNewsList = [...newsList, ...data.hits];
+    let hits = data.hits.map(obj => (obj.upvote_count = 0));
+    hits = this.updateVoteList(data.hits);
+    this.updateHiddenList(JSON.parse(localStorage.getItem('hiddenListArray')), data);
+    const updatedNewsList = [...newsList, ...hits];
     this.setState({ newsList: updatedNewsList });
   }
 
-  vote(i) {
+  updateVoteList(newData) {
+    const updatedVotesArr = JSON.parse(localStorage.getItem('upvoteList'));
+    for (let index = 0; index < updatedVotesArr.length; index += 1) {
+      for (let secIndex = 0; secIndex < newData.length; secIndex += 1) {
+        if (updatedVotesArr[index].id === newData[secIndex].objectID) {
+          newData[secIndex].upvote_count = updatedVotesArr[index].count;
+        }
+      }
+    }
+    return newData;
+  }
+
+  updateHiddenList(hiddenItems, data) {
+    if (hiddenItems) {
+      const objIdsArr = [];
+      for (let index = 0; index < hiddenItems.length; index += 1) {
+        objIdsArr.push(hiddenItems[index].objectID);
+      }
+      for (let item = 0; item < objIdsArr.length; item += 1) {
+        for (let secItem = 0; secItem < data.hits.length; secItem += 1) {
+          if (data.hits[secItem].objectID === objIdsArr[item]) {
+            data.hits.splice(secItem, 1);
+          }
+        }
+      }
+    }
+    return data.hits;
+  }
+
+  vote(item) {
+    const upvotedObjArr = !JSON.parse(localStorage.getItem('upvoteList')) ? [] : JSON.parse(localStorage.getItem('upvoteList'));
     const { newsList } = this.state;
     const newCountedArray = [...newsList];
-    newCountedArray[i].upvote_count += 1;
+    newCountedArray[item].upvote_count += 1;
+    const upvotedObj = {
+      id: newCountedArray[item].objectID,
+      count: newCountedArray[item].upvote_count,
+    };
+    // push this object in array only if that doesnt exist
+    for (let index = 0; index < upvotedObjArr.length; index += 1) {
+      if (upvotedObjArr[index].id === upvotedObj.id) {
+        upvotedObjArr.splice(index, 1);
+      }
+    }
+    upvotedObjArr.push(upvotedObj);
     this.setState({ newsList: newCountedArray });
+    localStorage.setItem('upvoteList', JSON.stringify(upvotedObjArr));
   }
 
   hide(index, item) {
-    const { hiddenListArray, newsList } = this.state;
+    const { newsList } = this.state;
+    const hiddenListArray = !localStorage.getItem('hiddenListArray') ? [] : JSON.parse(localStorage.getItem('hiddenListArray'));
     const newHiddenRowList = [...newsList];
     hiddenListArray.push(item);
     newHiddenRowList.splice(index, 1);
@@ -68,14 +105,14 @@ class NewsListComponent extends Component {
     const { newsList } = this.state;
     return (
       <div className="NewsList">
-        <table className="responsive-table">
-          <thead>
-            <tr>
-              <th>top</th>
-              <th>New</th>
-            </tr>
-          </thead>
-          <tbody>
+        <div className="responsive-table">
+          <div className="thead">
+            <div className="row">
+              <div className="head">top</div>
+              <div className="head">New</div>
+            </div>
+          </div>
+          <div>
             {
               newsList.map((item, i) => (
                 <div key={item.objectID} className="item">
@@ -85,7 +122,9 @@ class NewsListComponent extends Component {
                   <div className="content">
                     {item.upvote_count}
                   </div>
-                  <div role="button" tabIndex="0" className="content clickable" onClick={this.vote.bind(this, i)} onKeyDown={this.vote.bind(this, i)}>^</div>
+                  <div role="button" tabIndex="0" className="content clickable" onClick={this.vote.bind(this, i)} onKeyDown={this.vote.bind(this, i)}>
+                    <span className="carat"> ^ </span>
+                  </div>
                   <a className="content" href={item.url}>
                     {item.title}
                   </a>
@@ -104,8 +143,8 @@ class NewsListComponent extends Component {
                 </div>
               ))
             }
-          </tbody>
-        </table>
+          </div>
+        </div>
         <div className="btnContainer">
           <button type="button" onClick={this.loadMore.bind(this)}>More</button>
         </div>
