10111a5b9a2e7c1287f4b2a939342fda3422f1db
Raunak Kothari <raunak.kothari@publicissapient.com>
2020-04-01 05:17:33 +0530

    converted api path to secure https

--- src/helper/newHitsApi.js
+++ src/helper/newHitsApi.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const newHitsApiCall = pageNumber => (
-  axios.get(`http://hn.algolia.com/api/v1/search_by_date?tags=story&page=${pageNumber}`)
+  axios.get(`https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${pageNumber}`)
 );
 
 export default newHitsApiCall;
--- src/helper/topHitsApi.js
+++ src/helper/topHitsApi.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const topHitsApiCall = pageNumber => (
-  axios.get(`http://hn.algolia.com/api/v1/search?tags=story&page=${pageNumber}`)
+  axios.get(`https://hn.algolia.com/api/v1/search?tags=story&page=${pageNumber}`)
 );
 
 export default topHitsApiCall;
