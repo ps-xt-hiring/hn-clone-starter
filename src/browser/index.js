6070e9fdfa8cb4fe77f2b2aac3cb7ca556781a39
bionik93 <know.pranjul@gmail.com>
2020-04-30 16:29:37 +0530

    React-SSR-App-initialisation

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import App from "../shared/App";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from '../serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
